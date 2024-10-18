508975683fe6110c16e02a8ec47b5664ff55454b
stlouky <j.stloukall@gmail.com>
2024-10-19 00:34:41 +0200

    oprav MindFrame

--- framework/src/logger.c
+++ framework/src/logger.c
@@ -1,4 +1,4 @@
-#include "logger.h"
+#include "../include/logger.h"
 #include <time.h>
 #include <stdarg.h>
 
--- framework/src/test_framework.c
+++ framework/src/test_framework.c
@@ -1,4 +1,4 @@
-#include "test_framework.h"
+#include "../include/test_framework.h"
 
 /** @brief Globální proměnná pro sledování počtu spuštěných testů. */
 int tests_run = 0;
